5d9e322a988f6a7fb56ada6db848237c016a83bb
AdityaBahl <adityabahl12345@gmail.com>
2022-07-16 21:16:36 +0530

    Checks Sorted or not

--- recursion/Sorted_check.java
+++ recursion/Sorted_check.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//this program checks
+//this program checks whether
 public class Sorted_check {
     public static int check(int a[], int i) {
         if (i == a.length - 1)
