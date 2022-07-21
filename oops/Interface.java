9393b23ee7db1334455de2f6be348dd5aea27e77
AdityaBahl <adityabahl12345@gmail.com>
2022-07-21 15:28:30 +0530

    Interfacing in classes

--- oops/Interface.java
+++ oops/Interface.java
@@ -1,4 +1,4 @@
-//this program is an example of
+//this program is an example of inter
 interface Animal {// there can be no constructor in interface
     public void walk();// nor a non abstract function
 }
