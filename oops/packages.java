8952f12fbcb805cfc41146c0ed8d6258070d8c10
AdityaBahl <adityabahl12345@gmail.com>
2022-07-22 18:09:45 +0530

    An example showcasing importing of Packages

--- oops/packages.java
+++ oops/packages.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import bank.*;
 
-//this
+//this program
 public class Packages {
     public static void main(String[] args) {
         bank.Account account1 = new bank.Account();
