b7aa6e71862f91cb4c73ad4f73fbbc8ee150573b
AdityaBahl <adityabahl12345@gmail.com>
2022-07-26 23:27:37 +0530

    Advanced Pattern 2

--- patterns/advancedpattern2.java
+++ patterns/advancedpattern2.java
@@ -1,4 +1,4 @@
-//this program is for advanced pattern
+//this program is for advanced pattern 2
 public class advancedpattern2 {
     public static void main(String[] args) {
         int n = 5, k = n - 1;
