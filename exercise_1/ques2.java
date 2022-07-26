59b8493203cc8b6d13b7ec4c4e118b04fada2aec
AdityaBahl <adityabahl12345@gmail.com>
2022-07-27 00:41:22 +0530

    Question 2 File

--- exercise_1/ques2.java
+++ exercise_1/ques2.java
@@ -1,17 +1,21 @@
+
+//
 import java.util.Scanner;
-public class ques2{
-    public static void oddsum(int x){
-        int n=0;
-        for(int i=1;i<=x;i++){
-            if(i%2!=0){
-                n+=i;
+
+public class ques2 {
+    public static void oddsum(int x) {
+        int n = 0;
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0) {
+                n += i;
             }
         }
-        System.out.print("Sum of odd num is: "+n);
+        System.out.print("Sum of odd num is: " + n);
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         oddsum(n);
         sc.close();
     }
