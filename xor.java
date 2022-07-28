fa4922c671305837c7f7d189ea21a1e6f943a331
AdityaBahl <adityabahl12345@gmail.com>
2022-07-28 22:55:01 +0530

    XOR bit manipulation

--- xor.java
+++ xor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//This program is showing XOR
+//This program is showing XOR bit
 public class xor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
