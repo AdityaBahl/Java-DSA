a199453c94ae779d1ff50ff47b223b9d59358975
AdityaBahl <adityabahl12345@gmail.com>
2022-07-29 22:05:03 +0530

    bit manipulation 2

--- bit_manipulation2.java
+++ bit_manipulation2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//this program showcases bit manipula
+//this program showcases bit manipulati
 
 //get bit,set bit,clear bit,update bit
 //get bit operation- to point at a given bit
