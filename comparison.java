6be611aa2d1cf950d84ceb991cf180fcd0ba6466
AdityaBahl <adityabahl12345@gmail.com>
2022-08-02 20:35:15 +0530

    Comparison Operators

--- comparison.java
+++ comparison.java
@@ -1,5 +1,5 @@
 import java.util.*;
-//this program showcases comparison operat
+//this program showcases comparison operato
 
 public class comparison {
     public static void main(String[] args) {
