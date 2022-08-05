5526212d3ca0782bb4bf8cded064ada448f38d75
AdityaBahl <adityabahl12345@gmail.com>
2022-08-05 22:34:06 +0530

    Factorial Recursion

--- factorialrecur.java
+++ factorialrecur.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//this program showcases factorial recursio
+//this program showcases factorial recursion
 
 public class factorialrecur {
     public static void printfact(int n) {
