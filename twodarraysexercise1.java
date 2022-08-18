bfc767e8d4c2476dbd0244244eb3d5f93e06e981
AdityaBahl <adityabahl12345@gmail.com>
2022-08-18 22:42:36 +0530

    2D Array Exercise

--- twodarraysexercise1.java
+++ twodarraysexercise1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//this program showcases a 2-D array 
+//this program showcases a 2-D array e
 
 public class twodarraysexercise1 {
     public static void main(String[] args) {
